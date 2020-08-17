00cd8f1d8f6609bb61a3522e72403b9942165856
Yash-Kr <64688647+Yash-Kr@users.noreply.github.com>
2020-08-17 16:49:27 +0530

    add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+function on()
+{
+document.getElementById("bulb1").src="https://d14nx13ylsx7x8.cloudfront.net/lesson_image_blocks/assets/000/001/413/original/pic_bulbon.gif";
+}
+
+function off()
+{
+document.getElementById("bulb1").src="https://www.w3schools.com/js/pic_bulboff.gif";
+}
